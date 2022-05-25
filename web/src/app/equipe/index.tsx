6da971769aeddd63b838bb278537a1b12aa889c6
Olimpio Pimenta Barros <olimpiodx@gmail.com>
2022-05-25 12:41:30 -0300

    Adicionando loading

--- web/src/app/equipe/index.tsx
+++ web/src/app/equipe/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { api, IEquipeModel, LS } from '../utils';
+import Spinner from './Spinner';
+
 
 const EquipeView: React.FC = () => {
+    const [loading, setLoading] = React.useState(true);
     const [models, setModels] = React.useState<IEquipeModel[]>([]);
 
     const inputRef = React.useRef<HTMLInputElement>(null);
@@ -12,6 +15,8 @@ const EquipeView: React.FC = () => {
             .then((response) => {
                 localStorage.setItem(LS.EQUIPES, JSON.stringify(response.data));
                 setModels(response.data);
+            }).finally(() => {
+                setLoading(false);
             });
     }, []);
 
@@ -25,17 +30,27 @@ const EquipeView: React.FC = () => {
     React.useEffect(load, [load]);
 
     const handleSortear = React.useCallback(() => {
+        setLoading(true)
         api.post(`/api/equipe/sortear`)
             .then((response) => {
                 load();
             });
     }, [load]);
 
+    if (loading) {
+        return (<>
+            <h1>
+                <Link to='/'>Voltar</Link> / Equipe
+            </h1>
+            <h5 style={{ alignItems: "center" }}>Carregando <Spinner /></h5>
+        </>)
+    }
+
     return (<>
         <h1>
             <Link to='/'>Voltar</Link> / Equipe
         </h1>
-        <ul style={{ display: "flex", flexWrap: "wrap", flexDirection: "row" }}>
+        <ul style={{ display: "flex", flexWrap: "wrap", flexDirection: "row", listStyle: 'none' }}>
             {models.map((model) => {
                 return (<li key={model.id} style={{ width: "40%", margin: 10 }}>
                     <input value={model.name} />
